529d9b7b5512b089abc397b5c9e869018e5d581c
asrithnaverse <asrithnaverse@gmail.com>
2026-05-01 16:32:02 +0530

    Implement selection sort in ascending order

---
+++ selction sort ascending order.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n,j,index,arr[n];
+    cout<<"Enter the size of the array";
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(i=0;i<n-1;i++)
+    {
+        index=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[index])
+            {
+                index=j;
+            }
+        }
+        swap(arr[i],arr[index]);
+    }
+    for(i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
